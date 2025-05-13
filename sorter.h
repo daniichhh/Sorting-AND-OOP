20a51d4a71d1bb760f99f3a377aac26dc0dd6a59
daniichhh <itsdaniich@gmail.com>
2025-05-13 22:10:58 +0700

    Update sorter.h

--- sorter.h
+++ sorter.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef SORTER_H
+#define SORTER_H
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,25 +13,33 @@ public:
     virtual void sort(string key, int n) = 0;
 
 protected:
-    bool compare(City a, City b, string key) {
-        if (key == "name") return a.name < b.name;
-        if (key == "country") return a.country < b.country;
-        if (key == "lat") return a.lat < b.lat;
-        if (key == "lng") return a.lng < b.lng;
-        if (key == "population") return a.population < b.population;
-        return false;
-    }
+    bool compare(City a, City b, string key);
 };
 
+class BubbleSorter : public Sorter {
+public:
+    void sort(std::string key, int n) override;
+};
 
+class SelectionSorter : public Sorter {
+public:
+    void sort(std::string key, int n) override;
+};
 
-// ------------------- Bubble Sort -------------------
+class MergeSorter : public Sorter {
+public:
+    void sort(std::string key, int n) override;
+private:
+    void mergeSort(vector<City>& arr, string key, int left, int right);
+    void merge(vector<City>& arr, string key, int left, int mid, int right);
+};
 
+class QuickSorter : public Sorter {
+public:
+    void sort(std::string key, int n) override;
 
-// ------------------- Selection Sort -------------------
-
-
-// ------------------- Merge Sort -------------------
-
-
-// ------------------- Quick Sort -------------------
+private:
+    void quickSort(vector<City>& arr, string key, int low, int high);
+    int partition(vector<City>& arr, string key, int low, int high);
+};
+#endif // SORTER_H
