62efeaabc256d207e5b25818cece9fa720e4a10e
daniichhh <itsdaniich@gmail.com>
2025-05-13 20:54:13 +0700

    Update bacafile.h

--- bacafile.h
+++ bacafile.h
@@ -1,21 +1,17 @@
 #ifndef BACAFILE_H
 #define BACAFILE_H
-
-#include <iostream>
 #include <string>
-#include <fstream>
 #include <vector>
-#include <algorithm>
-
 using namespace std;
+
 struct City {
     string name, country;
     double lat, lng;
     long population;
 };
 
-vector<string> getData(string dataRaw);
-vector<City> readFile(string filename, int n);
+vector<City> readFile(string filename, int k);
+vector<City> getData((string dataRaw);
 
 
 #endif // BACAFILE_H
