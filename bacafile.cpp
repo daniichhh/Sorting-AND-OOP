2066b5ebcf3b41ba57b495d8eb790b141689b0d7
daniichhh <itsdaniich@gmail.com>
2025-05-13 20:54:49 +0700

    Update bacafile.cpp

--- bacafile.cpp
+++ bacafile.cpp
@@ -20,40 +20,37 @@ vector<string> getData(string dataRaw) {
     return result;
 }
 
+bool isNumeric(const string& str) {
+    return !str.empty() && all_of(str.begin(), str.end(), ::isdigit);
+}
 
-vector<City> readFile(string filename, int n) {
+vector<City> readFile(string filename, int k) {
     vector<City> cities;
     ifstream file(filename);
     string line;
     int counter = 0;
 
-    getline(file, line);
+    getline(file, line); // header
 
-    while (getline(file, line) && counter < n) {
+    while (getline(file, line) && counter < k) {
         vector<string> fields = getData(line);
-
-        if (fields.size() < 11) continue;
+        if (fields.size() < 10) continue;
 
         City city;
         city.name = fields[1];
         city.country = fields[4];
         city.lat = stod(fields[2]);
-        //city.lng = stod(fields[3]);
-
-        city.population = fields[9].empty() ? 0 : stol(fields[9]);
         city.lng = stod(fields[3]);
-        /*try {
-    		city.population = fields[9].empty() ? 0 : stol(fields[9]);
-		} catch (invalid_argument&) {
-    		continue;
-		}*/
+
+        if (isNumeric(fields[9])) {
+            city.population = stol(fields[9]);
+        } else {
+            city.population = 0;
+        }
 
         cities.push_back(city);
         counter++;
-
     }
 
     return cities;
 }
-
-
